c1934a50ca6e74e360fd4cfdeabc2c41cb0ad704
Santiago Rosso <santiagorosso@live.com.ar>
2025-05-29 14:38:47 -0300

    Create user service

--- src/modules/user/application/repository/user.repository.ts
+++ src/modules/user/application/repository/user.repository.ts
@@ -3,10 +3,13 @@ import { User } from '../../domain/user.domain';
 export const USER_REPOSITORY = 'USER_REPOSITORY';
 
 export interface UserRepository {
-  create(newUser: User): Promise<User>;
+  create(newUser: Partial<Omit<User, 'id'>>): Promise<User>;
   findAll(options?: object): Promise<User[]>;
   findOneById(id: string): Promise<User | null>;
   findOneByEmail(email: string): Promise<User | null>;
-  update(id: string, userUpdated: User): Promise<User | null>;
+  update(
+    id: string,
+    userUpdated: Partial<Omit<User, 'id'>>,
+  ): Promise<User | null>;
   delete(id: string): Promise<void>;
 }
---
+++ src/modules/user/application/service/user.service.ts
@@ -0,0 +1,63 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { USER_REPOSITORY, UserRepository } from '../repository/user.repository';
+import { CreateUserDto } from '../dtos/create-user.dto';
+import { UpdateUserDto } from '../dtos/update-user.dto';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @Inject(USER_REPOSITORY)
+    private readonly userRepository: UserRepository,
+  ) {}
+
+  async findAll() {
+    return this.userRepository.findAll();
+  }
+
+  async findOneById(id: string) {
+    try {
+      const response = await this.userRepository.findOneById(id);
+      if (!response) {
+        throw new Error(`User with ID ${id} not found`);
+      }
+      return response;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async findOneByEmail(email: string) {
+    const response = await this.userRepository.findOneByEmail(email);
+    if (!response) {
+      throw new Error(`User with email ${email} not found`);
+    }
+    return response;
+  }
+
+  async create(user: CreateUserDto) {
+    try {
+      return this.userRepository.create(user);
+    } catch (error) {
+      console.error('Error creating user:', error);
+      throw new Error('Failed to create user');
+    }
+  }
+
+  async update(id: string, user: UpdateUserDto) {
+    try {
+      return this.userRepository.update(id, user);
+    } catch (error) {
+      console.error('Error updating user:', error);
+      throw new Error(`Failed to update user with ID ${id}`);
+    }
+  }
+
+  async delete(id: string) {
+    try {
+      return this.userRepository.delete(id);
+    } catch (error) {
+      console.error('Error deleting user:', error);
+      throw new Error(`Failed to delete user with ID ${id}`);
+    }
+  }
+}
