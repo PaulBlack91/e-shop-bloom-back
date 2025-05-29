6af95d750519f85c7f9a2ba99383f67180a4352c
Santiago Rosso <santiagorosso@live.com.ar>
2025-05-29 14:38:58 -0300

    Create user controller

---
+++ src/modules/user/interface/user.controller.ts
@@ -0,0 +1,47 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { UserService } from '../application/service/user.service';
+import { CreateUserDto } from '../application/dtos/create-user.dto';
+import { UpdateUserDto } from '../application/dtos/update-user.dto';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  async findAll() {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  async findOneById(@Param('id') id: string) {
+    return this.userService.findOneById(id);
+  }
+
+  @Get('email/:email')
+  async findOneByEmail(@Param('email') email: string) {
+    return this.userService.findOneByEmail(email);
+  }
+
+  @Post()
+  async create(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.userService.update(id, updateUserDto);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string) {
+    return this.userService.delete(id);
+  }
+}
