4c3d2f4a2094212144ab39a67bedb6b551c646f7
Paul Esperon <paul.esperonarg@hotmail.com>
2025-06-01 22:06:10 -0300

    Agregar tipos de Passport y Google OAuth al proyecto

--- src/modules/auth/application/strategies/google.strategy.ts
+++ src/modules/auth/application/strategies/google.strategy.ts
@@ -1,27 +1,81 @@
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
-import { Strategy, VerifyCallback } from 'passport-google-oauth20';
+import { Profile, Strategy } from 'passport-google-oauth20';
 
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
   constructor() {
     super({
-      clientID: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      clientID: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
       callbackURL: 'http://localhost:3000/auth/google/redirect',
       scope: ['email', 'profile'],
     });
   }
 
-  async validate(accessToken: string, refreshToken: string, profile: any, done: VerifyCallback): Promise<any> {
+  // aca va el await con despues lo agrego para consultar la base de datos
+  async validate(
+    accessToken: string,
+    refreshToken: string,
+    profile: Profile,
+  ): Promise<any> {
     const { name, emails, photos } = profile;
-    const user = {
-      email: emails[0].value,
-      firstName: name.givenName,
-      lastName: name.familyName,
-      picture: photos[0].value,
+
+    return {
+      email: emails?.[0]?.value ?? '',
+      firstName: name?.givenName ?? '',
+      lastName: name?.familyName ?? '',
+      picture: photos?.[0]?.value ?? '',
       accessToken,
     };
-    done(null, user);
   }
 }
+
+// -----------------------------------------------
+
+// import { PassportStrategy } from '@nestjs/passport';
+// import { Injectable } from '@nestjs/common';
+// import { Strategy, VerifyCallback } from 'passport-google-oauth20';
+
+// @Injectable()
+// export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
+//   constructor() {
+//     super({
+//       clientID: process.env.GOOGLE_CLIENT_ID,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//       callbackURL: process.env.CALLBACK_URL_GOOGLE,
+//       scope: ['email','profile'],
+//       accessType: 'offline',
+//       prompt: 'consent',
+//       profileFields: ['emails', 'name'],
+//     });
+//   }
+//   async validate(
+//     accessToken: string,
+//     refreshToken: string,
+//     profile: any,
+//     done: VerifyCallback,
+
+//   ): Promise<any> {
+
+//     const { name, emails } = profile;
+//     const user = {
+//       email: emails[0].value,
+//       firstName: name.givenName,
+//       lastName: name.familyName,
+
+//     };
+
+//     const payload = {
+//       user,
+//       accessToken,
+//       refreshToken,
+//     };
+
+//     if (!user.email) {
+//       return done(new Error('No se pudo obtener un email del usuario'), null);
+//     }
+
+//     done(null, payload);
+//   }
+// }
