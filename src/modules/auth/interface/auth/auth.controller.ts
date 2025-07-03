0656a546eb8377a2a88a5a0ba7a8538f9cda5079
Paul Yasser Esperon <paul.esperonarg@hotmail.com>
2025-07-02 22:11:48 -0300

    feat(auth): implement Google authentication redirect with user token

--- src/modules/auth/interface/auth/auth.controller.ts
+++ src/modules/auth/interface/auth/auth.controller.ts
@@ -1,8 +1,9 @@
 // infrastructure/auth/controllers/auth.controller.ts
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards, Res } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from '../../application/service/auth.service';
 import { GoogleUser } from 'src/modules/user/application/dtos/create-user.dto';
+import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
@@ -14,7 +15,17 @@ export class AuthController {
 
   @Get('google/callback')
   @UseGuards(AuthGuard('google'))
-  async googleAuthRedirect(@Req() req: { user: GoogleUser }) {
-    return this.authService.handleGoogleLogin(req.user);
+  async googleAuthRedirect(
+    @Req() req: { user: GoogleUser },
+    @Res() res: Response,
+  ) {
+    const result = await this.authService.handleGoogleLogin(req.user);
+
+    // Redireccionar al frontend con el token y datos del usuario
+    const token = result.access_token;
+    const userData = encodeURIComponent(JSON.stringify(result.user));
+    const redirectUrl = `http://localhost:5174/auth/callback?token=${token}&user=${userData}`;
+
+    return res.redirect(redirectUrl);
   }
 }
